813549d94b8397dde0392452194f8b1d84dff169
z-26 <mdzeeshan5197@gmail.com>
2022-10-12 21:01:44 +0530

    first commit

---
+++ controllers/usercontroller.go
@@ -0,0 +1,88 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/z-26/go-gin-mongo/models"
+	"github.com/z-26/go-gin-mongo/services"
+)
+
+type UserController struct {
+	UserService services.UserService
+}
+
+func New(userservice services.UserService) UserController {
+	return UserController{
+		UserService: userservice,
+	}
+}
+func (uc *UserController) CreateUser(ctx *gin.Context) {
+	var user models.User
+	// below code binds JSON payloads coming from the POST /create endpoint to the user struct
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	err := uc.UserService.CreateUser(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
+}
+func (uc *UserController) GetUser(ctx *gin.Context) {
+	var username string = ctx.Param("name")
+	user, err := uc.UserService.GetUser(&username)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
+}
+
+func (uc *UserController) GetAll(ctx *gin.Context) {
+	users, err := uc.UserService.GetAll()
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, users)
+}
+
+func (uc *UserController) UpdateUser(ctx *gin.Context) {
+	var user models.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	err := uc.UserService.UpdateUser(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
+}
+
+func (uc *UserController) DeleteUser(ctx *gin.Context) {
+	var username string = ctx.Param("name")
+	err := uc.UserService.DeleteUser(&username)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
+}
+
+func (uc *UserController) RegisterUserRoutes(rg *gin.RouterGroup) {
+	fmt.Printf("url %v", rg)
+	// Grouping user routes under one name "user"
+	userroute := rg.Group("/user")
+	fmt.Println("url 2", userroute)
+	userroute.POST("/create", uc.CreateUser)
+	userroute.GET("/get/:name", uc.GetUser)
+	userroute.GET("/getall", uc.GetAll)
+	userroute.PATCH("/update", uc.UpdateUser)
+	userroute.DELETE("/delete/:name", uc.DeleteUser)
+}
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/z-26/go-gin-mongo/controllers"
+	"github.com/z-26/go-gin-mongo/services"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var (
+	server      *gin.Engine
+	us          services.UserService
+	uc          controllers.UserController
+	ctx         context.Context
+	userc       *mongo.Collection
+	mongoclient *mongo.Client
+	err         error
+)
+
+func init() {
+	ctx = context.TODO()
+
+	mongoconn := options.Client().ApplyURI("mongodb://localhost:27017")
+	mongoclient, err = mongo.Connect(ctx, mongoconn)
+	if err != nil {
+		log.Fatal("error while connecting with mongo", err)
+	}
+	err = mongoclient.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal("error while trying to ping mongo", err)
+	}
+
+	fmt.Println("mongo connection established")
+
+	userc = mongoclient.Database("userdb").Collection("users")
+	us = services.NewUserService(userc, ctx)
+	uc = controllers.New(us)
+	server = gin.Default()
+}
+
+func main() {
+	defer mongoclient.Disconnect(ctx)
+
+	basepath := server.Group("/v1")
+	uc.RegisterUserRoutes(basepath)
+
+	log.Fatal(server.Run(":9090"))
+
+}
